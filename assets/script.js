eb7cbddfb9ed5c22fe6bd87340fc7922706b6a53
Lonnie Root <root.lonnie@gmail.com>
2022-12-30 14:59:16 -0500

    current completed tasks

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,7 @@
 // in the html.
 $(document).ready(function () {
 
-  // TODO: Add code to display the current date in the header of the page.
-
-  var today = dayjs();
-$('#currentDay').text(today.format('MMM D, YYYY'));
+  
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -15,16 +12,56 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+//Saves to local storage
+  $(".saveBtn").on("click", function (){
+    var time = $(this).parent().attr("id")
+    var descriptionValue = $(this).siblings(".description").val()
+    localStorage.setItem(time, descriptionValue)
+  })
+// TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+//How to make values persist after page reload(do for each hour)
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   
+  function colorChange() {
+    var rightNow = dayjs().hour()
+    $(".time-block").each(function(){
+    var hardTime = parseInt($(this).attr("id").split("-")[1])
+
+    //to determine time color
+    if (hardTime < rightNow) {
+      $(this).addClass("past")
+
+    } else if (hardTime === rightNow) {
+      $(this).removeClass("past")
+      $(this).addClass("present")
+
+   } else {
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).addClass("future")
+   }
+    
+    })
+  }
+  colorChange()
+  setInterval(colorChange, 15000)
+  
+  // TODO: Add code to display the current date in the header of the page.
+
+  var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+
+//Fun thing to clear schedule after a week
+setInterval(function(){
+  localStorage.clear
+}, 604800000)
 });
