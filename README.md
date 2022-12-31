8b2297bdbd4b1d952180550e0a6ad797ad5a7936
Lonnie Root <root.lonnie@gmail.com>
2022-12-30 19:16:22 -0500

    updated read-me and clean-ups

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Module05-Challenge
+
+## Description 
+
+User Story: AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively.
+
+-----------------------------------------------------------
+
+To meet the needs of this user story I created a simple calendar application that allows the user to enter and save events for each hour of a typical 9am-5pm work day. The information saved in local storage will also persist should the user exit and/or refresh the browser page. For visual asthetics the past hours on the calendar will be displayed in grey, the present hour will be displayed in red, and future hours will be displayed in green. As an added feature I coded in a function that will automatically clear the local storage every week. This feature can easily be altered should the user prefer the storage be cleared daily, monthly, or not at all.
+
+## Technology
+HTML, CSS, JavaScript
+
+## Credits
+N/A
+
+## License 
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,32 @@
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Ensures that the code isn't run until the browser has finished rendering 
+// all the elements in the html.
 $(document).ready(function () {
 
-  
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-//Saves to local storage
+//Click event that saves to local storage
   $(".saveBtn").on("click", function (){
     var time = $(this).parent().attr("id")
     var descriptionValue = $(this).siblings(".description").val()
     localStorage.setItem(time, descriptionValue)
   })
-// TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-//How to make values persist after page reload(do for each hour)
+
+// Ensures the saved values persist upon exiting browser and/or reloading page
   $("#hour-9 .description").val(localStorage.getItem("hour-9"))
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
   
+//Function that uses Dayjs to interact with the HTML, it renders and updates
+//the color of each hour block to reflect past, present, and future.
   function colorChange() {
     var rightNow = dayjs().hour()
     $(".time-block").each(function(){
     var hardTime = parseInt($(this).attr("id").split("-")[1])
 
-    //to determine time color
     if (hardTime < rightNow) {
       $(this).addClass("past")
 
@@ -48,19 +39,16 @@ $(document).ready(function () {
     $(this).removeClass("present")
     $(this).addClass("future")
    }
-    
     })
   }
   colorChange()
   setInterval(colorChange, 15000)
   
-  // TODO: Add code to display the current date in the header of the page.
-
-  var today = dayjs();
+//Code that displays current date in the header
+ var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
-
-//Fun thing to clear schedule after a week
+//Fun thing to clear schedule after a week (can be altered)
 setInterval(function(){
   localStorage.clear
 }, 604800000)
